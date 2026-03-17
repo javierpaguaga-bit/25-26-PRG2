cd7e49f2a7e203d58e5753d86b91b67b9d7f72ea
javierpaguaga-bit <javier.paguaga@alumnos.uneatlantico.es>
2026-03-17 14:01:45 +0100

    Feat: Se han creado los metodos de Fraccion

---
+++ entregas/paguagaJavier/scr/fraccion/Cliente.java
@@ -0,0 +1,8 @@
+package entregas.paguagaJavier.scr.fraccion;
+
+public class Cliente {
+        public static void main(String[] args) {
+            Fraccion tresQuintos = new Fraccion(3, 5);
+            Fraccion dosCuartos = new Fraccion(2, 4);
+        }
+}
