4bc4b0998e19a55ccf9377fa1ad23ae62dab5e5a
javierpaguaga-bit <javier.paguaga@alumnos.uneatlantico.es>
2026-04-17 11:44:17 +0200

    feat: mostrarResumen Recorda arreglar despues codigo

--- entregas/paguagaJavier/scr/Caja.java
+++ entregas/paguagaJavier/scr/Caja.java
@@ -2,6 +2,10 @@ package entregas.paguagaJavier.scr;
 
 class Caja {
 
+    public Caja(int i) {
+        //TODO Auto-generated constructor stub
+    }
+
     public boolean estaLibre() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'estaLibre'");
@@ -20,6 +24,11 @@ class Caja {
     public void mostrar() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+    }
+
+    public void obtenerPersonasAtendidas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obtenerPersonasAtendidas'");
     }
 
 }
--- entregas/paguagaJavier/scr/Cola.java
+++ entregas/paguagaJavier/scr/Cola.java
@@ -3,6 +3,8 @@ package entregas.paguagaJavier.scr;
 class Cola {
 
     public Object quitarCLiente;
+    public Object obtenerMinutosSinCLientes;
+    public Object obtenerCantidad;
 
     public void registrarEstado() {
         // TODO Auto-generated method stub
@@ -22,6 +24,16 @@ class Cola {
     public void mostrar() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+    }
+
+    public Object obtenerMinutosSinCLientes() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obtenerMinutosSinCLientes'");
+    }
+
+    public Object obtenerCantidad() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obtenerCantidad'");
     }
 
 }
